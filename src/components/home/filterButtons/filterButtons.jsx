672f85742e09dfb22b6269a7c7d5ef89a4adc6d3
TomasSandoval <tomassandoval079@gmail.com>
2023-05-09 14:08:55 -0300

    buttons filters fixed

--- src/components/home/filterButtons/filterButtons.jsx
+++ src/components/home/filterButtons/filterButtons.jsx
@@ -7,9 +7,12 @@ export default function FilterButtons(){
     
     return (
         <div className="filter-buttons-container">
+            <h3>Categories</h3>
+            <div className="button-container">
             {
                 category.map((c,index) => <Link to={`/categories/${c.name}`} key={index} className="button-filter"><img className="filter-image" src={c.image}/><p key={index} className="filter-title">{c.name}</p></Link>)
             }
+            </div>
         </div>
     )
 }
