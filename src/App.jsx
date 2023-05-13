fa7c4172c8d2989d6aa54fb373e9aa9475565c6a
Jhonn <jhonndiaz58@gmail.com>
2023-05-12 23:17:41 -0400

    estilos-nav-carrusel-imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/home/home';
-import FormRegister from './components/form/form';
-import CategoriesProduct from './components/CategoriesProduct/CategoriesProduct';
-import Categories from './components/Categories/Categories';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/home";
+import FormRegister from "./components/form/form";
+import CategoriesProduct from "./components/CategoriesProduct/CategoriesProduct";
+import Categories from "./components/Categories/Categories";
 import Detail from "./components/Detail/Detail";
 import "./App.css";
-import FormUserLogin from './components/formUserLogin/formUserLogin';
-
+import FormUserLogin from "./components/formUserLogin/formUserLogin";
 
 function App() {
   return (
-        <>
-        <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/carroBuy' />
-        <Route path='/formRegister' element={<FormRegister/>}/>            
-        <Route path='/formLogin' element={<FormUserLogin/>}/>       
-        <Route path='/categories/:name' element={<CategoriesProduct/>}/>
-        <Route path='/categories' element={<Categories/>}/>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carroBuy" />
+        <Route path="/formRegister" element={<FormRegister />} />
+        <Route path="/formLogin" element={<FormUserLogin />} />
+        <Route path="/categories/:name" element={<CategoriesProduct />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/Detail/:id" element={<Detail />} />
-        </Routes>
-        </>
-  )
-
+      </Routes>
+    </>
+  );
 }
 export default App;
