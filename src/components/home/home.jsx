64497d7a3ab5390ce88617ac4ff07e03c04904d2
Tomás Sandoval <120404416+TomSandoval@users.noreply.github.com>
2023-05-22 21:47:37 -0300

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,9 +11,28 @@ import { checkExpiration, cleanProducts } from "../../redux/actions";
 export default function Home() {
   const dispatch = useDispatch();
 
+      useEffect(()=>{
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = decodeURIComponent(urlParams.get("token"));
+        const email = decodeURIComponent(urlParams.get("email"));
+        const username = decodeURIComponent(urlParams.get("username"));
+        const tokenExpiration = decodeURIComponent(urlParams.get("tokenExpiration"));
+        const roll = decodeURIComponent(urlParams.get("roll"));
+    
+
+        // Guardar los datos en el localStorage
+        localStorage.setItem("token", token);
+        localStorage.setItem("email", email);
+        localStorage.setItem("username", username);
+        localStorage.setItem("tokenExpiration", tokenExpiration);
+        localStorage.setItem("roll", roll);
+
+        navigate("/")
+
+    },[])
+
   useEffect(() => {
     const token = window.localStorage.getItem('token');
-
     if(token){
       dispatch(checkExpiration())
     }
